fd20c5243aaaf58fda2a479478b47f0b3cdf410f
Stuart Grimshaw <grimshaw@grimshaw.com>
2016-01-13 08:36:11 +1300

    add PlotBoddy template

---
+++ Plot Buddy Template/Plot Buddy WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Plot Buddy WatchKit Extension
+//
+//  Created by Stuart Grimshaw on 24/11/15.
+//  Copyright © 2015 Stuart Grimshaw. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Plot Buddy Template/Plot Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plot Buddy
+//
+//  Created by Stuart Grimshaw on 24/11/15.
+//  Copyright © 2015 Stuart Grimshaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
